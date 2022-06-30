3ea0c10468c978075d74608b7283f7072a5ef71f
josecas <jmcg0901@gmail.com>
2022-06-30 09:34:02 -0700

    Added liking post feature and HUD when composing a post

---
+++ Instagram/Models/Likes.h
@@ -0,0 +1,16 @@
+//
+//  Likes.h
+//  Instagram
+//
+//  Created by Jose Castillo Guajardo on 6/29/22.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Likes : PFObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/Models/Likes.m
@@ -0,0 +1,12 @@
+//
+//  Likes.m
+//  Instagram
+//
+//  Created by Jose Castillo Guajardo on 6/29/22.
+//
+
+#import "Likes.h"
+
+@implementation Likes
+
+@end
